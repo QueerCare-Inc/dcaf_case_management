74ffef9cee705d573ea7e1c8eba2c41ff41afc30
kmskog <kntn.ket@gmail.com>
2025-03-01 14:42:39 -0800

    fix typo

--- db/migrate/20250301221253_add_times_to_practical_supports.rb
+++ db/migrate/20250301221253_add_times_to_practical_supports.rb
@@ -3,6 +3,6 @@ class AddTimesToPracticalSupports < ActiveRecord::Migration[7.2]
     add_column :practical_supports, :start_time, :datetime
     add_column :practical_supports, :end_time, :datetime
 
-    remove_column practical_supports, :support_date, :date
+    remove_column :practical_supports, :support_date, :date
   end
 end
